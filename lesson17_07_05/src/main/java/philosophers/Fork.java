5cc5f0ab94e3632c33408e4c2d1c36f03aea29dc
Mihail Poplavkov <mihail.poplavkov@gmail.com>
2017-07-10 17:58:37 +0300

    Philosophers without deadlock

--- lesson17_07_05/src/main/java/philosophers/Fork.java
+++ lesson17_07_05/src/main/java/philosophers/Fork.java
@@ -1,14 +1,29 @@
 package philosophers;
 
 import lombok.Getter;
+import lombok.SneakyThrows;
 
 @SuppressWarnings("WeakerAccess")
 @Getter
 public class Fork {
     private static int counter = 1;
     private String name;
+    private boolean isTaken;
 
     public Fork() {
         name = String.format("Fork#%d", counter++);
+    }
+
+    @SneakyThrows
+    synchronized void take() {
+        while (isTaken) {
+            wait();
+        }
+        isTaken = true;
+    }
+
+    synchronized void release() {
+        isTaken = false;
+        notifyAll();
     }
 }
--- lesson17_07_05/src/main/java/philosophers/Philosopher.java
+++ lesson17_07_05/src/main/java/philosophers/Philosopher.java
@@ -1,5 +1,7 @@
 package philosophers;
 
+import lombok.SneakyThrows;
+
 @SuppressWarnings("WeakerAccess")
 public class Philosopher {
     private static int counter = 1;
@@ -13,20 +15,26 @@ public class Philosopher {
         this.right = right;
     }
 
+    @SneakyThrows
     public void eat() {
-        System.out.printf("%s: trying to take left fork - %s\n", name, left.getName());
-        synchronized (left) {
+        while (true) {
+            System.out.printf("%s: trying to take left fork - %s\n", name, left.getName());
+            left.take();
             System.out.printf("%s: left fork taken\n", name);
-            try {
+            Thread.sleep(100);
+            if (right.isTaken()) {
+                System.out.printf("%s: release left fork\n", name);
+                left.release();
                 Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                continue;
             }
             System.out.printf("%s: trying to take right fork - %s\n", name, right.getName());
-            synchronized (right) {
-                System.out.printf("%s: right fork taken\n", name);
-                System.out.printf("%s: eating...\n", name);
-            }
+            right.take();
+            System.out.printf("%s: right fork taken\n", name);
+            System.out.printf("%s: eating...\n", name);
+            left.release();
+            right.release();
+            break;
         }
     }
 }
